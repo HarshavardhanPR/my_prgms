495e6fe44aa688b45bb4aa00863c5dd13543ecaf
harshavardhanPR <prharshavardhan730@gmail.com>
2024-04-02 14:58:10 +0530

    The modified calculator.py file is created with modulo function

---
+++ operators.py
@@ -0,0 +1,14 @@
+def add(a,b):
+  print("The Sum is",a+b)
+
+def sub(a,b):
+  print("The Difference of",a," and ",b,"is",a-b)
+
+def mul(a,b):
+  print("The Sum is",a*b)
+
+def div(a,b):
+  print("The Sum is",a/b)
+
+def rem(a,b):
+    print("The Remainder is",a%b)
