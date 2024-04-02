2fa2cb99373d33c55d13b22b523ba71d05f1892d
harshavardhanPR <prharshavardhan730@gmail.com>
2024-04-02 15:24:22 +0530

    always running

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,21 @@
 
 import operators as o
-a=float(input("Enter a : "))
-b=float(input("Enter b : "))
-op=input("Enter the operator : ")
+while True:
+
+  a=float(input("Enter a : "))
+  b=float(input("Enter b : "))
+  op=input("Enter the operator : ")
 
 
-if(op=='+'):
-  o.add(a,b)
-elif(op=='-'):
-  o.sub(a,b)
-elif(op=='*'):
-  o.mul(a,b)
-elif(op=='/'):
-  o.div(a,b)
-elif(op=='%'):
-  o.rem(a,b)
-else:
-  print("Invalid input")
+  if(op=='+'):
+    o.add(a,b)
+  elif(op=='-'):
+    o.sub(a,b)
+  elif(op=='*'):
+    o.mul(a,b)
+  elif(op=='/'):
+    o.div(a,b)
+  elif(op=='%'):
+    o.rem(a,b)
+  else:
+    print("Invalid input")
